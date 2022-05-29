705c492ca8be6e08d1eb2db029afa0b212851049
mhmdfathan <102466798+mhmdfathan@users.noreply.github.com>
2022-05-29 17:27:09 +0700

    Update GUI.py

--- Tugas Akhir/GUI.py
+++ Tugas Akhir/GUI.py
@@ -1,20 +1,25 @@
-#Tugas TA
-#Muhamad Ibnu Fadhil
+#Tugas TA PDKP
+#Muhammad Fathan Mubiina
 
 #List Modul yang digunakan
 #1. Modul 1 : Variable
 #2. Modul 2 : if-else
-#3. Modul 3 : Perulangan for
-#4. Modul 4 : Function
-#5. Modul 5 : OOP
+#3. Modul 3 : Perulangan (loop)
+#4. Modul 4 : Function & method
+#5. Modul 5 : OOP 1 (class, constructor)
 #6. Modul 8 : GUI
-
 
 
 from tkinter import *
 from solve import solve
 
 #inisialisasi gui tkinter
+class root():
+    
+    def __init__(self, root):
+        self.root = root
+        
+
 root = Tk()
 root.title("Fat's Sudoku Solver")
 root.geometry("324x550")
@@ -31,7 +36,7 @@ solvedLabel.grid(row=15, column=1, columnspan=10, pady=5)
 
 cells = {}
 
-#validasi kolom
+#validasi panjang angka (satu grid hanya berisi 1 angka)
 def ValidateNumber(P):
     
     out = (P.isdigit() or P == "") and len(P) < 2
@@ -39,9 +44,7 @@ def ValidateNumber(P):
 
 reg = root.register(ValidateNumber)
 
-
-
-
+#inisialisasi grid sudoku 3x3
 def draw3x3Grid(row, column, bgcolor):
     for i in range(3):
         for j in range(3):
@@ -49,6 +52,7 @@ def draw3x3Grid(row, column, bgcolor):
             e.grid(row=row+i+1, column=column+j+1, sticky="nsew", padx=1, pady=1, ipady=5)
             cells[(row+i+1, column+j+1)] = e
 
+#inisialisasi grid sudoku 9x9
 def draw9x9Grid():
     color="#D0ffff"
     for rowNo in range(1, 10, 3):
@@ -59,6 +63,7 @@ def draw9x9Grid():
             else:
                 color = "#D0ffff"
 
+#fungsi untuk mereset papan sudoku
 def clearValues ():
     errLabel.configure(text="")
     solvedLabel.configure(text="")
@@ -67,6 +72,7 @@ def clearValues ():
             cell = cells[(row, col)]
             cell.delete(0, "end")
 
+#fungsi untuk mem fetch data dari papan
 def getValues():
     board = []
     errLabel.configure(text="")
@@ -83,12 +89,14 @@ def getValues():
         board.append(rows)
     updateValues(board)
 
+#tombol untuk solve, dan reset papan
 btn = Button(root, command=getValues, text="Solve", width=10)
 btn.grid(row=20, column=1, columnspan=5, pady=20)
 
 btn = Button(root, command=clearValues, text="Clear", width=10)
 btn.grid(row=20, column=5, columnspan=5, pady=20)
 
+#fungsi untuk mengupdate tampilan papan setelah solusi ditemukan
 def updateValues(s):
     sol = solve(s)
     if sol != "no":
